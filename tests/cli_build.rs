210c0764bec5ce017747f03ba92799d35b901f56
faithleysath <120073078+faithleysath@users.noreply.github.com>
2026-05-21 10:16:25 +0800

    Cover code gutter while scrolling

--- tests/cli_build.rs
+++ tests/cli_build.rs
@@ -72,7 +72,8 @@ fn init_and_build_site() {
     assert!(main_css.contains(".code-block"));
     assert!(main_css.contains(".code-block pre"));
     assert!(main_css.contains("position: sticky"));
-    assert!(main_css.contains("left: 1rem"));
+    assert!(main_css.contains("padding-left: 1rem"));
+    assert!(main_css.contains("left: 0"));
 }
 
 #[test]
